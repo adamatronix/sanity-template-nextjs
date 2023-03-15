9050ea4366df9ce988213ae12bc268f047384067
adamatronix <you@example.com>
2023-03-15 11:01:39 +0800

    setup Use svgr webpack for NextJS and set compiler to styled-components

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,18 @@ const config = {
       },
     ],
   },
+  compiler: {
+    styledComponents: true
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
 
+    return config
+  },
   reactStrictMode: true,
 }
 
