688296de6be57cdcfbcb637cfc31bc260fdd6fb1
adamatronix <you@example.com>
2023-03-15 14:20:56 +0800

    setup Add simple nav links on home and about page

--- src/page/AboutPage.tsx
+++ src/page/AboutPage.tsx
@@ -1,5 +1,6 @@
-import {PageData} from './types'
+import { Link } from 'components/Link/Link'
 
+import {PageData} from './types'
 export function AboutPage(props: {data: PageData | null}) {
   const {data} = props
 
@@ -8,6 +9,8 @@ export function AboutPage(props: {data: PageData | null}) {
       <div className="max-w-xl m-auto">
         <div className="prose dark:prose-invert">
           <h1 className="text-xl font-extrabold tracking-tight">About</h1>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+          <Link href="/">Go to home</Link>
         </div>
       </div>
     </div>
--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'components/Link/Link'
+
 import {PageData} from './types'
 
 export function HomePage(props: {data: PageData | null}) {
@@ -8,6 +10,8 @@ export function HomePage(props: {data: PageData | null}) {
       <div className="max-w-xl m-auto">
         <div className="prose dark:prose-invert">
           <h1 className="text-xl font-extrabold tracking-tight">Homepage</h1>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+          <Link href="/about">Go to about</Link>
         </div>
       </div>
     </div>
