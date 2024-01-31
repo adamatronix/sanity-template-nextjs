e42ed9abd36a67df30ce15e44ca5898249a22d5c
adamatronix <adam@adamatronix.com>
2024-01-31 13:30:53 +0800

    A-cb2 Create article template pages

---
+++ src/page/ArticlePage.tsx
@@ -0,0 +1,16 @@
+import {ArticleData} from './types'
+
+export function ArticlePage(props: {data: ArticleData | null}) {
+  const {data} = props
+
+  return (
+    <div className="p-5">
+      <div className="max-w-xl m-auto">
+        <div className="prose dark:prose-invert">
+          <h1 className="text-xl font-extrabold tracking-tight">{data?.title}</h1>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/page/query.ts
+++ src/page/query.ts
@@ -39,3 +39,110 @@ export const PAGE_PATHS_QUERY = groq`
     'slug': slug.current
   }
 `
+
+export const ARTICLE_PATHS_QUERY = groq`
+  *[_type == 'article' && defined(slug.current)]{
+    'slug': slug.current
+  }
+`
+
+
+export const ARTICLE_DATA_QUERY = groq`
+  *[_type == 'article' && slug.current == $slug][0]{
+    ...,
+    hero {
+      mobileFeatured {
+        alt,
+        asset->{
+          ...,
+          metadata
+        }
+      },
+      desktopFeatured {
+        alt,
+        asset->{
+          ...,
+          metadata
+        }
+      }
+    },
+    articleModules[] {
+      _type == 'articleCarousel' => {
+         _key,
+         _type,
+         stack,
+         images[] {
+           alt,
+           asset->{
+             ...,
+             metadata
+           }
+         }
+       }, 
+       _type == 'articleCopyBody' => {
+        ...
+       },
+       _type == 'articleImage50' => {
+         ...,
+         image {
+           desktopFeatured {
+             alt,
+             asset->{
+               ...,
+               metadata
+             }
+           },
+           mobileFeatured {
+             alt,
+             asset->{
+               ...,
+               metadata
+             }
+           }
+         }
+       },
+       _type == 'articleImage5050' => {
+         ...,
+         images[] {
+           ...,
+           desktopFeatured {
+             alt,
+             asset->{
+               ...,
+               metadata
+             }
+           },
+           mobileFeatured {
+             alt,
+             asset->{
+               ...,
+               metadata
+             }
+           }
+         }
+       },
+       _type == 'articleImage90' => {
+         ...,
+         image {
+           desktopFeatured {
+             alt,
+             asset->{
+               ...,
+               metadata
+             }
+           },
+           mobileFeatured {
+             alt,
+             asset->{
+               ...,
+               metadata
+             }
+           }
+         }
+       },
+       _type == 'articleVideo' => {
+         ...
+       },
+    }
+  }
+`
